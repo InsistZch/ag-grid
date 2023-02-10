870b06b25d6ca4b4013e5f9c68cd6ed058ab5616
szy <1665802666@qq.com>
2023-02-10 21:49:23 +0800

    減少全局變量對運行環境的影響

--- js/main/index.js
+++ js/main/index.js
@@ -1,9 +1,9 @@
 /** @odoo-module **/
-import agGridTable from './ag-grid/ag-grid-table.js'
+import init_grid_options from './ag-grid/ag-grid-table.js'
 import agChart from './ag-chart/chart-option.js'
 // import dishDataGrid from './dish-data-gird/dish-table.js'
 
 
 export default {
-    agGridTable,agChart
+    init_grid_options,agChart
 }
