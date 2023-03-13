5f4e6c50dafcee91380f1a070c06cb5be7ae9f30
InsistZch <3232348794@qq.com>
2023-03-13 14:09:20 +0800

    修复onCellClicked

--- js/main/ag-grid/ag-grid-api.js
+++ js/main/ag-grid/ag-grid-api.js
@@ -1043,14 +1043,6 @@ const getRowStyle = params => {
 }
 
 const onCellClicked = params => {
-    if(params.colDef.field == "dish"){
-        // console.log(params)
-        if(Restrictions(params)) return;
-        if(params.data.configure || params.data.dish == "" || params.data.dish == undefined) return
-        const { dish_family_id } = index.dish_key.find(v => v.id == params.data.dish_key_id.id)
-        const arr = index.dish_family.filter(v => v.id == dish_family_id)
-        console.log(arr)
-    }
     
 }
 // const onPasteStart = params => {
