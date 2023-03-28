2825e7c48a30d5ba7166628c79005c400f38820c
InsistZch <3232348794@qq.com>
2023-03-28 14:35:59 +0800

    修改采购单功能，修改按钮样式

--- js/main/purchase/CustomColumnsToolPanel.js
+++ js/main/purchase/CustomColumnsToolPanel.js
@@ -3,12 +3,43 @@ class CustomColumnsToolPanel {
     init(params){
         const eDiv = document.createElement('div')
         console.log(params)
-
+        eDiv.classList.add('el_columns')
+        console.log(params.api.getColumns)
         eDiv.innerHTML = `
-        <label>
+        <label class="el_columns_item">
             <input type="checkbox"/>
-            <span></span>
+            <span>需求日期</span>
+        </label>
+        <label class="el_columns_item">
+            <input type="checkbox"/>
+            <span>需量</span>
+        </label>
+        <label class="el_columns_item">
+            <input type="checkbox"/>
+            <span>库存</span>
+        </label>
+        <label class="el_columns_item">
+            <input type="checkbox"/>
+            <span>标准单价</span>
+        </label>
+        <label class="el_columns_item">
+            <input type="checkbox"/>
+            <span>市场价</span>
+        </label>
+        <label class="el_columns_item">
+            <input type="checkbox"/>
+            <span>应下单</span>
+        </label>
+        <label class="el_columns_item">
+            <input type="checkbox"/>
+            <span>下单</span>
+        </label>
+        <label class="el_columns_item">
+            <input type="checkbox"/>
+            <span>需求日期</span>
         </label>`
+        
+        
         this.eDiv = eDiv
     }
 
