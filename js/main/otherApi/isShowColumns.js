b8a1795d3c6a1c22f1a92511ff8cb4a496ec4023
InsistZch <3232348794@qq.com>
2023-03-30 00:00:43 +0800

    修复成本错误

--- js/main/otherApi/isShowColumns.js
+++ js/main/otherApi/isShowColumns.js
@@ -46,7 +46,6 @@ export default {
                         {
                             colId: item[0],
                             hide: !item[1].hide,
-                            width: 10,
                         }
                     ]
                 });
