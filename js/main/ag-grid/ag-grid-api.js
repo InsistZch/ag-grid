f0eb0f5f572e5edee28e9eb7d190ce560ae77f9e
InsistZch <3232348794@qq.com>
2023-04-20 19:23:36 +0800

    配量汇总删除bug

--- js/main/ag-grid/ag-grid-api.js
+++ js/main/ag-grid/ag-grid-api.js
@@ -364,7 +364,8 @@ const onCellValueChanged = async (e, gridOptions) => {
         
         let cancelDelete = true
         let material_data = d1.split(' ')
-        if(material_data.length < e.oldValue.split(" ").length){
+        if(material_data.length < (e.oldValue + " ").split(" ").length){
+            console.log('1')
             await new Promise((resolve, reject) => {
                 customFromDom({
                     parent: '#isDeleteRow',
